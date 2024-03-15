ff99bf8d573997850f7e1d31336e7a00e15513e2
Mrudul John Mathews <67854062+mruduljohn@users.noreply.github.com>
2024-03-16 00:56:50 +0530

    rect fix

--- code.ts
+++ code.ts
@@ -1,34 +1,24 @@
-// This plugin will open a window to prompt the user to enter a number, and
-// it will then create that many circles on the screen.
-
-// This file holds the main code for plugins. Code in this file has access to
-// the *figma document* via the figma global object.
-// You can access browser APIs in the <script> tag inside "ui.html" which has a
-// full browser environment (See https://www.figma.com/plugin-docs/how-plugins-run).
-
-// This shows the HTML page in "ui.html".
-figma.showUI(__html__);
-// Calls to "parent.postMessage" from within the HTML page will trigger this
-// callback. The callback will be passed the "pluginMessage" property of the
-// posted message.
-figma.ui.onmessage =  (msg: {type: string, count: number}) => {
-  // One way of distinguishing between different types of messages sent from
-  // your HTML page is to use an object with a "type" property like this.
-  if (msg.type === 'create-circles') {
-    const nodes: SceneNode[] = [];
-    for (let i = 0; i < msg.count; i++) {
-      const circle = figma.createEllipse();
-      circle.x = i * 150;
-      circle.y = i * 150;
-      circle.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
-      circle.resize(100, 100); // Adjust the size of the circle as needed
-      figma.currentPage.appendChild(circle);
-      nodes.push(circle);
-    }
-    figma.currentPage.selection = nodes;
-    figma.viewport.scrollAndZoomIntoView(nodes);
+if (figma.editorType === 'figma') {
+  // This plugin creates 5 rectangles on the screen.
+  const numberOfRectangles = 5
+  
+  // This file holds the main code for the plugins. It has access to the *document*.
+  // You can access browser APIs such as the network by creating a UI which contains
+  // a full browser environment (see documentation).
+  
+  const nodes: SceneNode[] = [];
+  for (let i = 0; i < numberOfRectangles; i++) {
+    const rect = figma.createRectangle();
+    rect.x = i * 150;
+    rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
+    figma.currentPage.appendChild(rect);
+    nodes.push(rect);
   }
-  // Make sure to close the plugin when you're done. Otherwise, the plugin will
+  figma.currentPage.selection = nodes;
+  figma.viewport.scrollAndZoomIntoView(nodes);
+  
+  // Make sure to close the plugin when you're done. Otherwise the plugin will
   // keep running, which shows the cancel button at the bottom of the screen.
   figma.closePlugin();
-};
+  
+  }
