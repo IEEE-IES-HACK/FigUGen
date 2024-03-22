9666a4d7a5a73fe98d5150cfa069c54102140ee9
abdulhakkeempa <abdulhakkeempa2002@gmail.com>
2024-03-23 00:11:29 +0530

    some bug fixes

--- code.ts
+++ code.ts
@@ -6,7 +6,7 @@ const openai = new OpenAI({
   apiKey: process.env['OPENAI_API_KEY'], // This is the default and can be omitted
 });
 
-export default async function main(prompt: string) {
+async function main(prompt: string) {
   const completion = await openai.chat.completions.create({
     messages: [
       {"role": "system", "content": "You are a helpful assistant. You are great at converting user prompts into figma api codes for generating components in figma.For reference use https://www.figma.com/plugin-docs/api/global-objects/ and all the sub directories. You will respond the correct api call code which i can directly pass to api to create components. Nothing else."},
@@ -15,8 +15,6 @@ export default async function main(prompt: string) {
     model: 'gpt-3.5-turbo',
     stream: false
   });
-
-  console.log(completion.choices[0]?.message?.content);
 }
 
 figma.showUI(__html__);
@@ -24,7 +22,6 @@ figma.ui.onmessage = (msg) => {
 
 if (msg.type === 'run') {
   main(msg.prompt).then((response) => {
-    console.log("Hello");
   });
 }
 
