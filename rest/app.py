16b491e2de6cfe51889b46ded8234f645d260185
abdulhakkeempa <abdulhakkeempa2002@gmail.com>
2024-03-23 16:53:58 +0530

    created rest api

---
+++ rest/app.py
@@ -0,0 +1,39 @@
+from flask import Flask, request
+from openai import OpenAI
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+print(os.getenv("OPENAI_API_KEY"))
+
+client = OpenAI(
+    api_key=os.getenv("OPENAI_API_KEY")
+)
+
+def PrompttoFigma(text):
+    try:
+        response = client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant. You are great at converting user prompts into figma api codes for generating components in figma.For reference use https://www.figma.com/plugin-docs/api/global-objects/ and all the sub directories. You will respond the correct api call code which i can directly pass to api to create components. Nothing else."},
+                {"role": "user", "content": text},
+            ],
+            stream=False,  # Setting stream to False
+        )
+        figmacode = response.choices[0].message.content
+        return figmacode
+    except Exception as error:
+        print(f"An error occurred: {error}")
+        return None
+    
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST'])
+def post_request():
+  data = request.get_json()
+  prompt = data['prompt']
+  return PrompttoFigma(prompt)
+
+if __name__ == '__main__':
+  app.run()
